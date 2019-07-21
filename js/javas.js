b94af678b31040a8cff31b3febca495cc756c440
Khalil Fawzy <khalilbrainiarc1@gmail.com>
2019-07-21 14:29:24 +0300

    introducing the java script to toggle the what we do icons

---
+++ js/javas.js
@@ -0,0 +1,9 @@
+$('.one').on('click', function(){
+    $('.hide-1').toggle();
+});
+$('.two').on('click', function(){
+    $('.hide-2').toggle();
+});
+$('.three').on('click', function(){
+    $('.hide-3').toggle();
+});
